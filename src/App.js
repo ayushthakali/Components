756ab484966436ca63e10a77fa1fceea217fa6b0
chilliflakes5790 <thakaliaayush@gmail.com>
2025-06-20 11:01:03 +0545

    feat: icons added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import Button from "./Button";
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
 
 function App() {
   return (
     <div>
       <div>
-        <Button primary>Click Me!</Button>
+        <Button primary outline rounded>
+          <GoBell className="mr-1"/>
+          Click Me!
+        </Button>
       </div>
       <div>
-        <Button secondary>Buy</Button>
+        <Button secondary outline>
+          <GoCloudDownload />
+          Buy
+        </Button>
       </div>
       <div>
-        <Button success> Sell</Button>
+        <Button success>
+          <GoDatabase /> Sell
+        </Button>
       </div>
       <div>
         <Button warning>Come on</Button>
