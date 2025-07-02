5dc0e0db9ccbfe560a7abb9737d6c24368e2ff2b
chilliflakes5790 <thakaliaayush@gmail.com>
2025-07-02 21:46:12 +0545

    feat:SortableTable,js added

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,4 +1,4 @@
-import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
 
 function TablePage() {
   const data = [
@@ -9,18 +9,29 @@ function TablePage() {
   ];
 
   const config = [
-    { label: "Name ", render: (fruit) => fruit.name },
-    { label: "Color", render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div> },
-    { label: "Score", render: (fruit) => fruit.score , header:()=><th className="bg-red-500">Score</th>},
+    {
+      label: "Name ",
+      render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name,
+    },
+    {
+      label: "Color",
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
+    },
+    {
+      label: "Score",
+      render: (fruit) => fruit.score,
+      sortValue: (fruit) => fruit.score,
+    },
   ];
 
-  const keyFn =(fruit)=>{
-      return fruit.name;
-  }
+  const keyFn = (fruit) => {
+    return fruit.name;
+  };
 
   return (
     <div>
-      <Table data={data} config={config} keyFn={keyFn} />
+      <SortableTable data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }
